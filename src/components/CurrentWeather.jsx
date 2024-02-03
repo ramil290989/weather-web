5b610a6fb8c61eaab88bcb08e2f9a86c030d8d97
ramil290989 <garifullinramil290989@gmail.com>
2024-02-03 18:58:13 +0500

    remove old

--- src/components/CurrentWeather.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const CurrentWeather = () => (
-  <div className='flex border-2 rounded-md'>
-    погода сейчас
-  </div>
-);
-
-export default CurrentWeather;
--- src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Header = () => (
-  <header className="container">
-    <p className='text-center text-2xl'>Дюртюли</p>
-  </header>
-);
-
-export default Header;
