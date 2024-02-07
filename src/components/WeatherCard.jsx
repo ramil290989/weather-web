dbafc99d82d3ca8f294f95e22fe482bb911b3ae3
ramil290989 <garifullinramil290989@gmail.com>
2024-02-07 15:47:50 +0500

    add Now button

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import {
+  Button,
   Col,
   Image,
   Row,
 } from 'react-bootstrap';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectNowWeather } from '../slices/selectors.js';
+import { actions as selectedActions } from '../slices/selectedSlice.js';
 
 const WeatherCard = () => {
+  const dispatch = useDispatch();
   const data = useSelector((state) => state.selected.data);
+  const dataNow = useSelector(selectNowWeather);
   const {
     dateDescription,
     dateLocal,
@@ -28,13 +33,40 @@ const WeatherCard = () => {
     day: 'numeric',
   };
   return (
-    <Col xs={12} sm={10} md={9} lg={7} xl={6} xxl={5} className="border border-primary-subtle rounded p-1 p-md-3 mx-auto">
-      <p className="mb-0">
-        {date.toLocaleString('ru-RU', options)}
-      </p>
-      <p>
-        {dateDescription}
-      </p>
+    <Col
+      xs={12}
+      sm={10}
+      md={9}
+      lg={7}
+      xl={6}
+      xxl={5}
+      className="border border-primary-subtle rounded p-1 p-md-3 mx-auto shadow-sm"
+    >
+      <Row>
+        <Col>
+          <p className="mb-0">
+            {date.toLocaleString('ru-RU', options)}
+          </p>
+          <p className="mb-0">
+            {dateDescription}
+          </p>
+        </Col>
+        <Col className="col-auto">
+          {
+            dateDescription === 'сейчас'
+              ? null
+              : (
+                <Button
+                  variant="outline-info"
+                  size="sm"
+                  onClick={() => dispatch(selectedActions.showData(dataNow))}
+                >
+                  Погода сейчас
+                </Button>
+              )
+          }
+        </Col>
+      </Row>
       <Row className="flex-column flex-md-row">
         <Col md={4} className="text-center text-md-start">
           <span className="fs-1">
